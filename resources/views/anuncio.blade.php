4f8b561c9f696025e1d1c03aae9e51502f40b6bd
Angel Compean <43077937+aCompean@users.noreply.github.com>
2019-11-09 17:48:44 -0600

    Update anuncio.blade.php

--- resources/views/anuncio.blade.php
+++ resources/views/anuncio.blade.php
@@ -6,7 +6,62 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Registro de Prestamista</title>
 </head>
+
 <body>
+<header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center position-relative">
+
+
+          <div class="site-logo">
+            <a href="index.php" class="text-black"><span class="text-primary">AJUVEI</a>
+          </div>
+
+          <div class="col-12">
+            <nav class="site-navigation text-right ml-auto " role="navigation">
+              <form action="">
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  @guest
+                  @if (Route::has('register'))
+                  <a href="{{ route('login') }}" class="btn btn-primary btn-lg" role="button"
+                    aria-pressed="true">Iniciar
+                    sesión</a>
+                  <a class="btn btn-primary btn-lg" role="button" aria-pressed="true"
+                    href="{{ route('register') }}">Registrarse</a>
+                  @endif
+                  @else
+                  <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                        Cerrar sesion
+                      </a>
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                      </form>
+                    </div>
+                  </li>
+                  @endguest
+                </ul>
+              </form>
+            </nav>
+
+          </div>
+
+          <div class="toggle-button d-inline-block d-lg-none"><a href="#"
+              class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+
+    </header>
     <form>
         Nombre/s:<br>
         <input type="text" name="nombre"><br>
@@ -25,7 +80,7 @@
             <option value="carpintero">Carpinteria</option>
         </select><br>
         Numero telefonico:<br>
-        <input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"> Ej: (833)(***)(***)<br>
+        <input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="(833)(***)(***)"><br>
         Descripción: <br>
         <input type="text" name="descripcion"placeholder="Describir su manera de trabajar"><br>
         Dirección de su lugar de trabajo<br>
@@ -39,5 +94,5 @@
         <input type="number" name="cp"min="5" max="5"><br>
         <input type="submit"value="Registrar"> <br>
     </form>
-</body>
+  </body>
 </html>

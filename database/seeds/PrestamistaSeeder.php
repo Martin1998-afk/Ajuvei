7793b3fb951a522c1d52c17c9543ac80a71b701d
Martin Octavio <a172553@une.edu.mx>
2019-11-09 22:31:50 -0600

    checa esto gustavo plis

--- database/seeds/PrestamistaSeeder.php
+++ database/seeds/PrestamistaSeeder.php
@@ -1,5 +1,6 @@
 <?php
 use Faker\Factory as Faker;
+use App\Prestamista;
 use Illuminate\Database\Seeder;
 
 class PrestamistaSeeder extends Seeder
@@ -12,5 +13,20 @@ class PrestamistaSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
+        for($i=0; $i<5; $i++){
+            $prestamista = new Prestamista();
+            $prestamista->name = $faker->name;
+            $prestamista->apellidop = $faker->lastName;
+            $prestamista->apellidom = $faker->lastName;
+            $prestamista->email = $faker->unique()->email;
+            $prestamista->service = $faker->sentence($nbWords = 6, $variableNbWords = true);
+            $prestamista->cellphone = $faker->tollFreePhoneNumber;
+            $prestamista->description = $faker->paragraph($nbSentences = 3, $variableNbSentences = true);
+            $prestamista->colonia = $faker->citySuffix;
+            $prestamista->calle = $faker->streetName;
+            $prestamista->numext = $faker->buildingNumber;
+            $prestamista->cp = $faker->postcode;
+            $prestamista->save();
+        }
     }
 }
--- database/seeds/ServiceUsersSeeders.php
+++ database/seeds/ServiceUsersSeeders.php
@@ -14,16 +14,16 @@ class ServiceUsersSeeders extends Seeder
      */
     public function run()
     {
-        $users = User::all();
+        $prestamistas = User::all();
         $services = Service::all();
         $faker = Faker::create();
-        $users = sizeof($users);
+        $prestamistas = sizeof($prestamistas);
         $services = sizeof($services);
         $r = random_int(1,20);
         for ($i=0; $i < $r; $i++) { 
             $relation = new ServiceUser();
             $relation->service_id = $faker->numberBetween(1,$services);
-            $relation->user_id = $faker->numberBetween(1,$users);
+            $relation->prestamista_id = $faker->numberBetween(1,$prestamistas);
             $relation->active = true;
             $relation->save();
         }
